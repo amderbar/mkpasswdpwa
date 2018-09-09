72677832e8a80f14acc2931c217b580da43b82ea
amderbar <amderbar@gmail.com>
2018-09-09 14:48:30 +0900

    feat: パッケージ追加
    * simple-json
    * web-storage
    * それらを使うモジュールの作成

---
+++ src/Mkpasswd/Data/Storage.purs
@@ -0,0 +1,19 @@
+module Mkpasswd.Data.Storage where
+
+import Prelude
+import Data.Either                 (Either, note)
+import Effect                      (Effect)
+import Foreign                     (MultipleErrors, ForeignError(..))
+import Simple.JSON                 as JSON
+import Web.HTML                    as WH
+import Web.HTML.Window             as WW
+import Web.Storage.Storage         as WS
+
+fetch :: forall t. JSON.ReadForeign t => String -> Effect (Either MultipleErrors t)
+fetch key = let e = ForeignError $ key <> ": No such key"
+             in (note (pure e) >=> JSON.readJSON)
+             <$> (WH.window >>= WW.sessionStorage >>= WS.getItem key)
+
+save :: forall t. JSON.WriteForeign t => String -> t -> Effect Unit
+save key d = WH.window >>= WW.sessionStorage >>= WS.setItem key (JSON.writeJSON d)
+
