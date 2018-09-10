eeef80e48bf8b40539e69e5f41aa744506896a8c
amderbar <amderbar@gmail.com>
2018-09-11 00:56:22 +0900

    feat: storageをlocalStorageに変更

--- src/Mkpasswd/Data/Storage.purs
+++ src/Mkpasswd/Data/Storage.purs
@@ -9,11 +9,13 @@ import Web.HTML                    as WH
 import Web.HTML.Window             as WW
 import Web.Storage.Storage         as WS
 
+storage :: WH.Window -> Effect WS.Storage
+storage = WW.localStorage
+
 fetch :: forall t. JSON.ReadForeign t => String -> Effect (Either MultipleErrors t)
 fetch key = let e = ForeignError $ key <> ": No such key"
              in (note (pure e) >=> JSON.readJSON)
-             <$> (WH.window >>= WW.sessionStorage >>= WS.getItem key)
+             <$> (WH.window >>= storage >>= WS.getItem key)
 
 save :: forall t. JSON.WriteForeign t => String -> t -> Effect Unit
 save key d = WH.window >>= WW.sessionStorage >>= WS.setItem key (JSON.writeJSON d)
-
